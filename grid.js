85aa343a417c9176218a409d8206679948a3d3a5
Kevin Pan <89890227+kpan02@users.noreply.github.com>
2025-03-19 00:49:59 -0400

    grid - basic page layout

---
+++ grid.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // This function will be expanded to load all photos in a grid format
+    function initPhotoGrid() {
+        const gridContainer = document.getElementById('photo-grid');
+        
+        // Placeholder for future implementation
+        gridContainer.innerHTML = '<div style="text-align: center; padding: 50px; width: 100%;">Photo grid coming soon!</div>';
+    }
+    
+    initPhotoGrid();
+});
